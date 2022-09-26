29c137ed472ad6ae3cf1e324a4f70fcebdccfa70
Solihbek4443 <gaffarovsolihbek@gmail.com>
2022-09-26 18:10:45 +0500

    ex 3.1

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// ex 1
+// let L=prompt("L enter length (sm)");
+// console.log(`complete metrs ${Math.trunc(L/100)} metr`);
+
+// ex 2
+// const M=prompt("M enter light");
+// console.log(`full tons ${Math.trunc(m/1000)} m`);
+
+// ex 3
+// let A=prompt("A enter number");
+// let B=prompt('B enter number (b<a)')
+// console.log(`A in corse section B in crose section ${Math.trunc (a/b)} can be placed ones`);
+
+// ex 4
+// let A=prompt("enter a two-digit number")
+// console.log(`number in tens place ${A[0]}`)
+// console.log(`a number in ones place ${A[1]}`)
+
+// ex 5
+// let a=promopt("enter a two-digit number")
+// console.log(`is the sum of the digits of this number ${a[0]*1+a[1]*1}`);
+
+// ex 6
+// const A=prompt("enter a two-digit number")
+// console.log(`the digit product ofthi number ${a[0]*1*a[1]*1}`);
+
+// ex 7
+// const a=prompt("enter a four-dight number")
+// console.log(`in this number ${Math.trunc(a/100)}there are hundreds`);
+
+// ex 8
+// const n=prompt("how many seconds have passed since the beginning of the day")
+// console.log(`since the day began ${Math.trunc (n/3600)} seconds ${Math.trunc((n%3600)/60)}a minute passed`);
